cbd5c021ff6b0e5cc07d403bb99e3f92f853fa53
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-10 01:26:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
+  word_arr = []
   arr = string.split(" ")
   arr.each do |word|
-   return  word_arr[word.reverse]
+   word_arr << word.reverse
  end
+ return word_arr.join(" ")
 end
